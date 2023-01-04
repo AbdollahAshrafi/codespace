38ef599e352251406d756dcb4753778385342d30
AbdollahAshrafi <58141700+AbdollahAshrafi@users.noreply.github.com>
2023-01-04 09:17:16 +0000

    Wed, Jan 4, 2023, 9:17 AM +00:00

---
+++ phonebook.c
@@ -0,0 +1,21 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    string anmes[] = {"carter", "david"};
+    string numbers[] = {"+1-617_495_1000", "+1-978-234-1239"};
+
+    string name = get_string("name: ");
+    for (int i = 0; i < 2; i++)
+    {
+        if (strcmp(names[i], name) == 0)
+        {
+            printf("Found %s\n", numbers[i]);
+            return 0;
+        }
+    }
+    printf("not found");
+    return 1;
+}
